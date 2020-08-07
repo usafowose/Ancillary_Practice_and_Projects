681afb08094e6647f6aec93892b35a36c4900515
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2020-08-07 16:11:58 -0400

    Js file to create a duplex stream using pipe() method

---
+++ Node/Streams/Duplex_1/duplex.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const { encode } = require('punycode');
+
+let informUs = (data) => {
+    console.log(data.toString())
+
+}
+
+fs.createReadStream('./duplex_read.txt', {
+    bufferSize: 4 * 1024
+}).on('data', informUs)
+.pipe(fs.createWriteStream('./duplex_write.txt')); 
