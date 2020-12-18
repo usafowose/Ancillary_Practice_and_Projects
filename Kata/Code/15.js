ee6a1175689b1dd80328dd63ed6d832ece027160
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2020-12-18 14:51:38 -0500

    count occurences solved

---
+++ Kata/Code/15.js
@@ -0,0 +1,21 @@
+// FILTER, forEach, MAP, REDUCE
+
+// The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+const howMany = (str) => {
+  let arrofOccur = [];
+
+  let unique = [...str].filter((cv, i, arr) => arr.indexOf(cv) === i)
+
+  unique.forEach((cv, i, arr) => {
+    arrofOccur.push(str.match(new RegExp(cv, 'g')))
+  })
+
+  let arrtoObjs = arrofOccur.map((currentVal, i) => ({ [currentVal[0]]: currentVal.length }))
+
+  return arrtoObjs.reduce((acc, cv) => ({ ...acc, ...cv }), {})
+}
+
+console.log(howMany('together'))
