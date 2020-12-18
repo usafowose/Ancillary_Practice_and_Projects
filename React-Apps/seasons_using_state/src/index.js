3a2ce5468caf906a7d76b2d003b8ac9f65184a75
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2020-12-18 14:51:01 -0500

    Completed seasons app

--- React-Apps/seasons_using_state/src/index.js
+++ React-Apps/seasons_using_state/src/index.js
@@ -1,32 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import SeasonDisplay from './SeasonDisplay';
+import "semantic-ui-css/semantic.min.css"
 
 class App extends React.Component {
+  // state can be intialized outside of constructor --often done...use w/o this outside of constructor
+  // if nothing other than state is intitialize dinside contructor, construc and props can be removed and state can be intialized like below. 
+  // babel will compile this to establish constructor, etc
 
   state = { lat: null, errorMessage: '' }
 
   render() {
     return (
-
       (this.state.errorMessage && !this.state.lat) ?
         <div>Error: {this.state.errorMessage}</div>
         : (this.state.lat && !this.state.errorMessage) ?
-          <div>Latitude: {this.state.lat}</div>
+          <SeasonDisplay lat={this.state.lat} />
           : <div>Loading!</div>
     )
   }
 
   componentDidMount() {
+    // grabbing currentLoc --one time process
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({ lat: position.coords.latitude }),
       err => this.setState({ errorMessage: err.message })
     );
+    console.log(this.state)
   }
 
   componentDidUpdate() {
+    console.log(this.state)
     console.log(`My component was just updated`)
   }
 }
 
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root')) 
--- React-Apps/seasons_using_state/src/seasonDisplay.js
+++ React-Apps/seasons_using_state/src/seasonDisplay.js
@@ -1,13 +1,39 @@
+import './SeasonDisplay.css'
 import React from 'react'
 
-const SeasonDisplay = () => {
+const seasonConfig = {
+  summer: {
+    text: `Let's Hit the Beach`,
+    iconName: `sun`
+  },
+  winter: {
+    text: `Brrrr. It's Chilly`,
+    iconName: `snowflake`
+  }
+}
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? `summer` : `winter`
+  }
+  else {
+    return lat > 0 ? 'winter' : `summer`
+  }
+}
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season]
   return (
-    <div>Season Display</div>
+    <div className={`season-display ${season}`}>
+      <i className={` massive icon icon-left ${iconName}`}></i>
+      <h1>{text}</h1>
+      <i className={`massive icon icon-right ${iconName}`}></i>
+    </div>
   )
 }
 
-
 export default SeasonDisplay
 
-window.navigator.geolocation
+
 
