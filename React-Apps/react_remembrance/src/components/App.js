314da50965a1c5c3cb017e2a6e24cde213a68a37
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-01-16 16:32:51 -0500

    Declared method to handle the score increment

--- React-Apps/react_remembrance/src/components/App.js
+++ React-Apps/react_remembrance/src/components/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Header from './Header'
+import Image from './ImageList'
 
 class App extends React.Component {
+  state = { score: 0 };
 
+  handleScoreIncrement = (e) => {
+    this.setState({ score: this.state.score + 1 })
+    console.log(this.state.score)
+  }
 
   render() {
 
     return (
       <div>
-        <Header />
+        <Header score={this.state.score} />
+        <Image increaseScore={this.handleScoreIncrement} />
       </div>
     )
 
