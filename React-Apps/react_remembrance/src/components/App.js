43b97aecdc4b5d70d9e42d3b58f109bf1554f319
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-01-16 16:01:05 -0500

    Connecting app comp with index to render content to screen

---
+++ React-Apps/react_remembrance/src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './Header'
+
+class App extends React.Component {
+
+
+  render() {
+
+    return (
+      <div>
+        <Header />
+      </div>
+    )
+
+  }
+};
+
+
+export default App; 
