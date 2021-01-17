d70754ded01b87d1a271217d6aae478fa059b90a
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-01-17 11:27:45 -0500

    Added logic to restart game

--- React-Apps/react_remembrance/src/components/App.js
+++ React-Apps/react_remembrance/src/components/App.js
@@ -6,6 +6,8 @@ import Image from './ImageList'
 // LOGIC 
 // 1. Keep Tabs of Score
 // 2. Restart Game Condition
+
+// TO-DO : Conditionally restart the game
 class App extends React.Component {
   state = { score: 0, };
 
@@ -15,14 +17,29 @@ class App extends React.Component {
     console.log(this.state.score)
   }
 
+  handleRestart = () => {
+    this.setState({ score: 0 })
+  }
+
+
   render() {
 
     return (
       <div>
-        <Header score={this.state.score} />
-        <Image increaseScore={this.handleScoreIncrement} />
-        <Image increaseScore={this.handleScoreIncrement} />
-        <Image increaseScore={this.handleScoreIncrement} />
+        <Header
+          score={this.state.score} />
+        <Image
+          restart={this.handleRestart}
+          increaseScore={this.handleScoreIncrement}
+        />
+        <Image
+          restart={this.handleRestart}
+          increaseScore={this.handleScoreIncrement}
+        />
+        <Image
+          restart={this.handleRestart}
+          increaseScore={this.handleScoreIncrement}
+        />
 
       </div>
     )
