1274cf902ee4d3853963bacc81899fbcf20ea4f9
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-01-17 21:33:36 -0500

    Mapping different image components to test state

--- React-Apps/react_remembrance/src/components/App.js
+++ React-Apps/react_remembrance/src/components/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import Header from './Header'
 import Image from './ImageList'
 
-
 // LOGIC 
 // 1. Keep Tabs of Score
 // 2. Restart Game Condition
 
 // TO-DO : Conditionally restart the game
 class App extends React.Component {
-  state = { score: 0, };
-
+  state = { score: 0 };
+  array = [1, 2, 3]
 
   handleScoreIncrement = (e) => {
     this.setState({ score: this.state.score + 1 })
@@ -21,25 +20,24 @@ class App extends React.Component {
     this.setState({ score: 0 })
   }
 
-
   render() {
 
     return (
       <div>
         <Header
           score={this.state.score} />
-        <Image
-          restart={this.handleRestart}
-          increaseScore={this.handleScoreIncrement}
-        />
-        <Image
-          restart={this.handleRestart}
-          increaseScore={this.handleScoreIncrement}
-        />
-        <Image
-          restart={this.handleRestart}
-          increaseScore={this.handleScoreIncrement}
-        />
+        {this.array.map((cv, i, arr) => {
+          return (
+            <div key={cv}>
+              <Image
+                score={this.state.score}
+                increaseScore={this.handleScoreIncrement}
+                restart={this.handleRestart}
+              />
+            </div>
+          )
+        })
+        }
 
       </div>
     )
