f2714160fedcd4b8092ad097979bfd30b8c4afd5
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-01-18 20:37:49 -0500

    Export array of Images for shuffling in app

---
+++ React-Apps/react_remembrance/src/components/ImageArray/index.js
@@ -0,0 +1,9 @@
+import Broccoli from './../../assets/Images/Broccoli1JPG.jpg'
+import Broccoli2 from './../../assets/Images/Broccoli2JPG.jpg';
+
+
+let ImageArray = [Broccoli, Broccoli2];
+
+
+
+export default ImageArray;
