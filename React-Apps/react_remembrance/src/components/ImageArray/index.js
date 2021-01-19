7a1d175f21a70d4a9d2867535a8a61596f7b7bee
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-01-19 17:58:05 -0500

    Exported full array of 16 images

--- React-Apps/react_remembrance/src/components/ImageArray/index.js
+++ React-Apps/react_remembrance/src/components/ImageArray/index.js
@@ -1,9 +1,23 @@
-import Broccoli from './../../assets/Images/Broccoli1JPG.jpg'
-import Broccoli2 from './../../assets/Images/Broccoli2JPG.jpg';
+import Broccoli from '../../assets/Images/Broccoli1JPG.jpg'
+import Broccoli2 from '../../assets/Images/Broccoli2JPG.jpg';
+import Broccoli3 from '../../assets/Images/Broccoli3JPG.jpg'
+import Broccoli4 from '../../assets/Images/Broccoli4JPG.jpg'
+import Broccoli5 from '../../assets/Images/Broccoli5JPG.jpg'
+import Broccoli6 from '../../assets/Images/Broccoli6.jpg'
+import Broccoli7 from '../../assets/Images/Broccoli7JPG.jpg'
+import Broccoli8 from '../../assets/Images/Broccoli8.jpg'
+import Broccoli9 from '../../assets/Images/Broccoli9JPG.jpg'
+import Broccoli10 from '../../assets/Images/Broccoli10.jpg'
+import Broccoli11 from '../../assets/Images/Broccoli11.jpg'
+import Broccoli12 from '../../assets/Images/Broccoli12.png'
+import Broccoli13 from '../../assets/Images/Broccoli13.jpg'
+import Broccoli14 from '../../assets/Images/Broccoli14.png'
+import Broccoli15 from '../../assets/Images/Broccoli15.jpg'
+import Broccoli16 from '../../assets/Images/Broccoli16.jpg'
 
 
-let ImageArray = [Broccoli, Broccoli2];
-
+let ImageArray = [Broccoli, Broccoli2, Broccoli3, Broccoli4, Broccoli5, Broccoli6, Broccoli7, Broccoli8, Broccoli9, Broccoli10, Broccoli11, Broccoli12, Broccoli13, Broccoli14, Broccoli15, Broccoli16];
 
 
 export default ImageArray;
+
