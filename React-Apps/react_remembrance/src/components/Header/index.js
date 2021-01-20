b74f2532e824ea1c3786918e839b9e183dcaf441
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-01-19 21:04:56 -0500

    Passed highScore as prop to header for dynamic rendering

--- React-Apps/react_remembrance/src/components/Header/index.js
+++ React-Apps/react_remembrance/src/components/Header/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const Header = ({ score }) => {
+const Header = ({ score, highScore }) => {
   return (
     <div>
       <header>
         <h4>React Rememberace</h4>
         <h1> Clicky game</h1>
         <p>Score: {score}</p>
+        <p>High Score: {highScore}</p>
       </header>
     </div>
   )
