d9c81ef3a1167da777b948d2395be04dc2a0b47e
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-01-31 19:41:36 -0500

    Adding algorith, to test if a string is a palindrome

---
+++ Kata/Code/19.js
@@ -0,0 +1,23 @@
+const isPalindrome = (str = '') => {
+  let string = [...str];
+  // first loop would start @ 0th index
+  // inner loop starts at the end
+  // check if the first val === last val 
+  // if so (increment i and decrment j) --> check again
+  // if not (return false)
+  // if i === str.length - 1 || j = string.length - 1 return true
+
+  let i = 0;
+  let j = string.length - 1
+
+  while (i < j && j <= string.length - 1) {
+    if (string[i] !== string[j]) return false;
+    if (i === j) break
+    i++;
+    j--
+  }
+  return ((i + j === string.length - 1) && j - i <= 1) ? true : false
+
+}
+
+console.log(isPalindrome('acecar'))
