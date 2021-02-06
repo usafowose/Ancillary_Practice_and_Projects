5d6d8d7ac930e7674d60b561fa8856f2229fdb4e
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-02-06 18:39:57 -0500

    Proj files for console hello world app

---
+++ C#/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace C_
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      System.String name = "Queen";
+      string[] people = { "Queen", "Andrew", "Mom" };
+      int[] nums = { 4, 5, 6 };
+
+      
+      Console.WriteLine($"{people[0]}");
+      Console.WriteLine($"{nums}");
+      Console.WriteLine($"{name}");
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
