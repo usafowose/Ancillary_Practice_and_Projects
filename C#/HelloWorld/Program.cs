7853f2717e61581ff0f5dc363c52eee2ccf60a73
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-02-06 20:02:32 -0500

    Using string interpolation and string escape sequences

--- C#/HelloWorld/Program.cs
+++ C#/HelloWorld/Program.cs
@@ -13,10 +13,13 @@ namespace C_
       System.String greeting = "Hello, World";
       // System.String newGreeting = greeting.Replace("World", name); 
 
-      Console.WriteLine(greeting.Contains(", "));
+      Console.WriteLine(greeting.Contains(", ")); //Should Return True
+      Console.WriteLine(greeting.Replace("World", name)); //
+      Console.WriteLine(greeting.IndexOf("Worl"));
 
+      var Queen = (firstName: "Queen", lastName: "Davison", age: 22, favFood: "Papa Johns");
 
-      Console.WriteLine(greeting.Replace("World", name));
+      Console.WriteLine($"{Queen.firstName} -- {Queen.lastName} \n Age: {Queen.age} \n\t FavFood: {Queen.favFood}");
     }
 
     static void NotMain1(string[] args)
