be6ed5ce7cc742fa777d97de4b5e2c3ed0716d0f
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-02-06 19:51:11 -0500

    str modifications using methods for console app

--- C#/HelloWorld/Program.cs
+++ C#/HelloWorld/Program.cs
@@ -6,15 +6,32 @@ namespace C_
   {
     static void Main(string[] args)
     {
-      System.String name = "Queen";
+      string name = "Queen";
+
+      Console.WriteLine($"Hello, my name is {name}");
+
+      System.String greeting = "Hello, World";
+      // System.String newGreeting = greeting.Replace("World", name); 
+
+      Console.WriteLine(greeting.Contains(", "));
+
+
+      Console.WriteLine(greeting.Replace("World", name));
+    }
+
+    static void NotMain1(string[] args)
+    {
+      string name = "Queen";
+
       string[] people = { "Queen", "Andrew", "Mom" };
       int[] nums = { 4, 5, 6 };
 
-      
-      Console.WriteLine($"{people[0]}");
-      Console.WriteLine($"{nums}");
       Console.WriteLine($"{name}");
+      Console.WriteLine($"My Girfriends Name Is {name} and her name is {name.Length} letters long");
+      Console.WriteLine($"{nums}");
       Console.WriteLine("Hello World!");
+      Console.WriteLine(people[1]);
+      Console.WriteLine($"{people[0]}");
     }
   }
 }
