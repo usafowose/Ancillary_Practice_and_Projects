5eb95f94c676760284cbed7901ec256b241e5ae3
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-02-08 17:52:45 -0500

    Minor changes brought on by VS references and mods to dll files

---
+++ C#/HelloWorld2/HelloWorld2/Program.cs
@@ -0,0 +1,14 @@
+using System; 
+
+namespace HelloWorld2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 18;
+            int b = 25;
+            Console.WriteLine(a + b); 
+        }
+    }
+}
