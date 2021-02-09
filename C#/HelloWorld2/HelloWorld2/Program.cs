5586eb3b6e6171dbe1c97fbcfa1876ac9bcd3bf7
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-02-09 13:57:28 -0500

    More work with double number type

--- C#/HelloWorld2/HelloWorld2/Program.cs
+++ C#/HelloWorld2/HelloWorld2/Program.cs
@@ -26,17 +26,33 @@ namespace HelloWorld2
 
             //Duplicating Overflow Condition (Wrap-Around)
             int overflow = max + 3;
-            Console.WriteLine($"An Example of Overflow: {overflow}");
+            Console.WriteLine($"An Example of Overflow: {overflow}\n");
 
             //Working With "Double" Numeric Type
             double A = 5;
-            double B = 4.5;
+            double B = 4;
             double C = 2;
             double D = (A + B) / C;
-            Console.WriteLine($"{A} plus {B} divided by {C} is {D}"); 
+            Console.WriteLine($"{A} plus {B} divided by {C} is {D} \n"); //returns 4.5 which includes the decimal portion of the quotient
 
+            //Another Example 
+            double A1 = 19;
+            double B1 = 23;
+            double C1 = 8;
+            double D1 = (A1 + B1) / C1;
+            Console.WriteLine(D1);
+
+            //Floats have much larger range of Max and Min values
+            double maxDouble = double.MaxValue;
+            double minDouble = double.MinValue;
+            double maxDoubleOverflow = maxDouble * 100; 
+            Console.WriteLine($"Max Double Number : {maxDouble} \nMin Double Number: {minDouble}\n Overflow: {maxDoubleOverflow}"); // Goes all the way to + and - 1.7976931348623157E+308 
+
+
+            double oneThird = 1.0 / 3.0;
+            Console.WriteLine(oneThird); 
         }
-
+        
         public void NewFunc()
         {
             Console.WriteLine("Hello Friends");
