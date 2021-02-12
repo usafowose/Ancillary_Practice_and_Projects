513ddfaf726855c3ba6be74d8feab30ed3d16263
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-02-11 23:16:26 -0500

    Prefizz coding challenge in C# and gitignore for blob C# config files

---
+++ C#/Kata_Coding_Challenges/Kata_Coding_Challenges/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Kata_Coding_Challenges
+{
+    //class Program
+    //{
+
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("Hello World!");
+    //    }
+    //}
+
+     public class Kata
+    {
+        public static int[] PreFizz(int n)
+        {
+            int start = 1;
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = start;
+                start++;
+            }
+            return array;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PreFizz(10));
+        }
+    }
+}
+
