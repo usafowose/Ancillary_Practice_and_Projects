668ab4cb5cbcb82bb3b30611a79ba82e4aa63149
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-02-12 10:36:39 -0500

    Added C# coding challenge to sum all values inclusively between two int

---
+++ C#/Kata_Coding_Challenges/GetSum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GetSum
+{
+    class Program
+    {
+
+        public static int GetSum(int num1, int num2)
+        {
+            int sum = 0;
+            int least = Math.Min(num1, num2), greatest = Math.Max(num1, num2);
+            do
+            {
+                sum += least;
+                least++;
+            } while (least <= greatest);
+            return sum; 
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetSum(3, -1));
+        }
+    }
+}
