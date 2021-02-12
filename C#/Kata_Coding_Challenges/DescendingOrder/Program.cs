92cd7e9a78392a9f151b963ec9d135a7a2bd7d1f
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-02-11 23:27:36 -0500

    Added static method to new challenge in C# to return descending order int input

---
+++ C#/Kata_Coding_Challenges/DescendingOrder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DescendingOrder
+{
+    class Program
+    {
+
+        // Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+        // Examples:
+        // Input: 42145 Output: 54421
+        public int Descending(int args)
+        {
+
+            return 5; 
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
