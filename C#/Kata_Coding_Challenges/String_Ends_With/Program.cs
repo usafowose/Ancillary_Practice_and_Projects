1c6d4b6524972ed7d8920e12e567109d5388d740
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-02-12 09:36:01 -0500

    Adding 'string ends with' coding challenge using .NET Class Library Methods

---
+++ C#/Kata_Coding_Challenges/String_Ends_With/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace String_Ends_With
+{
+    class Program
+    {
+        public static bool EndsWith(string str, string ending) => str.EndsWith(ending);
+        static void Main(string[] args)
+        {
+            Console.WriteLine(EndsWith("Johnny Dang", "ang"));
+        }
+    }
+}
