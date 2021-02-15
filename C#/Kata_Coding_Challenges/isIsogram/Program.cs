551ad7dbc8da7469dd2f63bb0daa9886423bb5f9
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-02-15 17:55:46 -0500

    Coding challenge for checking Isogram

---
+++ C#/Kata_Coding_Challenges/isIsogram/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace isIsogram
+{
+     class Program
+    {
+
+         static bool IsIsogram(string str)
+        {
+            // Code on you crazy diamond!
+            Char[] check = new Char[str.Length];
+            //Console.WriteLine(check[0]); 
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (Array.IndexOf(check, Char.ToUpper(str[i])) == -1)
+                {
+                
+                    check[i] = Char.ToUpper(str[i]);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+        static void Main()
+            {
+                Console.WriteLine("Hello World!");
+            Console.WriteLine(IsIsogram("Dermatooglyphics"));
+               
+            }
+
+    }
+}
