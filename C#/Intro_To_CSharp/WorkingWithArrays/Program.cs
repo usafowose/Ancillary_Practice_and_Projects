999a070dff40acafd8bd447a377ffd488f7f4a2c
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-02-17 00:00:34 -0500

    Added new prj 'Working With Arrays' to intialize and assing vals in arrays

---
+++ C#/Intro_To_CSharp/WorkingWithArrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkingWithArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
