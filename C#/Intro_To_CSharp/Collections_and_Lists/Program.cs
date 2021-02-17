78ca21e6f354ceb07e6d3d96b051b3ca39da008c
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-02-17 16:49:50 -0500

    Initializing List of type string and using inbuilt List methods

--- C#/Intro_To_CSharp/Collections_and_Lists/Program.cs
+++ C#/Intro_To_CSharp/Collections_and_Lists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic; 
 
 namespace Collections_and_Lists
 {
@@ -6,7 +7,28 @@ namespace Collections_and_Lists
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var names = new List<string> { "Andrew", "Queen", "Ashley" };
+   
+            //foreach(var name in names)
+            //{
+            //    Console.WriteLine($"Hello, {name.ToUpper()}!"); 
+            //}
+
+            names.Add("Maria");
+            names.Add("John");
+            names.Remove("Ashley");
+
+            names.ForEach(Console.WriteLine);
+
+            int length = names.Count;
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine($"Name {i+1}: {names[i]}\n");
+            }
+
+
+
         }
     }
 }
