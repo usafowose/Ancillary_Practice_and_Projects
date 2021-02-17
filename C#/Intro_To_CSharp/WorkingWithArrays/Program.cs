d3a363baeb25c8db2f05d2530727b662eb2cf923
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-02-17 00:06:01 -0500

    Adding class method to take an array and using foreach to print elems

--- C#/Intro_To_CSharp/WorkingWithArrays/Program.cs
+++ C#/Intro_To_CSharp/WorkingWithArrays/Program.cs
@@ -4,8 +4,29 @@ namespace WorkingWithArrays
 {
     class Program
     {
+
+        static string[] ArrayPractice()
+        {
+            string[] names = new string[5]; // allocates memory for string array containing 5 values; 
+            names[0] = "Andrew";
+            names[1] = "Fikayo";
+            names[2] = "Mobolaji";
+            names[3] = "Jimmy";
+            names[4] = "Simeon";
+
+            return names;
+        }
+
+        static void Enumerate(string[] names)
+        {
+            foreach (string name in names)
+            {
+                Console.WriteLine(name); 
+            }
+        }
         static void Main(string[] args)
         {
+            Enumerate(ArrayPractice()); 
             Console.WriteLine("Hello World!");
         }
     }
