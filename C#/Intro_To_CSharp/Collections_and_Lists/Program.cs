859104bcb1970c9894f04762a5fa8707dbde5953
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-02-17 14:50:13 -0500

    More manipulation of Arrays and new Lists_Collections project

---
+++ C#/Intro_To_CSharp/Collections_and_Lists/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Collections_and_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- C#/Intro_To_CSharp/WorkingWithArrays/Program.cs
+++ C#/Intro_To_CSharp/WorkingWithArrays/Program.cs
@@ -53,3 +53,4 @@ namespace WorkingWithArrays
         }
     }
 }
+ 
