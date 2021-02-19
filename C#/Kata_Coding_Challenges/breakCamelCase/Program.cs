3e70870aa174d8b039715e32873c65dd94e6b903
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-02-19 11:20:45 -0500

    Solved challenge to return string split by camel Case

--- C#/Kata_Coding_Challenges/breakCamelCase/Program.cs
+++ C#/Kata_Coding_Challenges/breakCamelCase/Program.cs
@@ -1,11 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace breakCamel
 {
+
+//Complete the solution so that the function will break up camel casing, using a space between words.
+//Example
+//solution("camelCasing")  ==  "camel Casing"
     class Program
     {
+        static string Solution(string input)
+        {
+            List<char> charList = new List<char>();
+
+            foreach(char letter in input)
+            {
+                if( letter == char.ToUpper(letter))
+                {
+                    charList.Add(' ');
+                    charList.Add(letter); 
+                } 
+                else
+                {
+                    charList.Add(letter); 
+                }
+            }
+
+            return new string(charList.ToArray()); 
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine(Solution("camelCaseCatAndDog")); 
             Console.WriteLine("Hello World!");
         }
     }
