62a77a74c180e15e782191922fe666334719502b
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-02-22 13:53:40 -0500

    Added instructions

--- Kata/Code/27.js
+++ Kata/Code/27.js
@@ -1,3 +1,9 @@
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
 
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
 
 const reverseWords = str => str.split(" ").map(cv => [...cv].reverse().join('')).join(' ');
+
+console.log(reverseWords("sihT si na !elpmaxe")); 
