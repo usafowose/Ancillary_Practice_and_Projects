c222982819bca6ed298e51e0bf8f010c9debb29f
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-02-22 13:00:55 -0500

    New Challenge To Reverse String in Place

---
+++ C#/Kata_Coding_Challenges/Reverse_Words/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+//Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+//Examples
+//"This is an example!" ==> "sihT si na !elpmaxe"
+//"double  spaces"      ==> "elbuod  secaps"
+namespace Reverse_Words
+{
+    class Program
+    {
+
+        static string Reverse_Words(string word_input)
+        {
+            List<char> Reversed = new List<char>();
+            List<char> thisWordReversed = new List<char>();
+            int start = 0; 
+            while (start <= word_input.Length - 1)
+            {
+                char thisChar = word_input[start]; 
+                 
+                if(thisChar== ' ')
+                {
+                    thisWordReversed.Add(thisChar);
+                    thisWordReversed.Reverse();
+                    Reversed.AddRange(thisWordReversed); 
+                }
+                thisWordReversed.Add(thisChar);
+                start++; 
+
+                
+        
+            }
+            return new string(Reversed.ToArray());  
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Reverse_Words("sihT si na !elpmaxe"));
+        }
+    }
+}
