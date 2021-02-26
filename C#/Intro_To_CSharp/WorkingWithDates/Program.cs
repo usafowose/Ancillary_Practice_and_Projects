f5db0b9d95cdbd10407a4bf11456c50abbb1ed6d
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-02-25 19:05:54 -0500

    Accessing properties of DateTime objects

--- C#/Intro_To_CSharp/WorkingWithDates/Program.cs
+++ C#/Intro_To_CSharp/WorkingWithDates/Program.cs
@@ -6,11 +6,28 @@ namespace WorkingWithDates
     {
         static void Main(string[] args)
         {
+            //Creating a custom DateTime object pointing to any point in time.
             DateTime dateTime = new DateTime(2021, 2, 24);
             Console.WriteLine(dateTime);
-
+            // Getting the current time when the code is run, down to the second. 
             DateTime now = DateTime.Now;
             Console.WriteLine(now);
+            //Getting the current day when the code is run, without including the time of day
+            DateTime today = DateTime.Today;
+            Console.WriteLine(today);
+
+            Console.WriteLine(today.ToLocalTime());
+            Console.WriteLine(today.Hour);
+            Console.WriteLine(today.Minute);
+            Console.WriteLine(today.DayOfWeek);
+
+            Console.WriteLine(now.ToLocalTime());
+            Console.WriteLine(now.Hour);
+            Console.WriteLine(now.Minute);
+            Console.WriteLine(now.DayOfWeek);
+
+
+
         }
     }
 }
