45b434601787af2e5abb831036aefa622f434882
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-02-28 14:13:44 -0500

    Using inbuilt object mthods to modify DateTime objects

--- C#/Intro_To_CSharp/WorkingWithDates/Program.cs
+++ C#/Intro_To_CSharp/WorkingWithDates/Program.cs
@@ -8,25 +8,39 @@ namespace WorkingWithDates
         {
             //Creating a custom DateTime object pointing to any point in time.
             DateTime dateTime = new DateTime(2021, 2, 24);
-            Console.WriteLine(dateTime);
+            //Console.WriteLine(dateTime);
             // Getting the current time when the code is run, down to the second. 
             DateTime now = DateTime.Now;
-            Console.WriteLine(now);
+            //Console.WriteLine(now);
             //Getting the current day when the code is run, without including the time of day
             DateTime today = DateTime.Today;
-            Console.WriteLine(today);
 
-            Console.WriteLine(today.ToLongDateString());
-            Console.WriteLine();
-            Console.WriteLine(today.Hour);
-            Console.WriteLine(today.Minute);
-            Console.WriteLine();
-            Console.WriteLine(today.DayOfWeek);
+            //Modifying DateTime Objects
+            DateTime tomorrow = now.AddDays(1);
+            DateTime yesterday = now.AddDays(-1);
 
             Console.WriteLine(now.ToLongDateString());
-            Console.WriteLine(now.Hour);
-            Console.WriteLine(now.Minute);
-            Console.WriteLine(now.DayOfWeek);
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+
+
+            //Console.WriteLine("------------------------");
+            //Console.WriteLine(today);
+            //Console.WriteLine(today.ToLongDateString());
+
+            //Console.WriteLine();
+            //Console.WriteLine(today.Hour);
+            //Console.WriteLine(today.Minute);
+            //Console.WriteLine();
+
+
+            //Console.WriteLine(today.DayOfWeek);
+
+            //Console.WriteLine(now.ToLongDateString());
+            //Console.WriteLine(now.Hour);
+            //Console.WriteLine(now.Minute);
+            //Console.WriteLine(now.DayOfWeek);
             
       
 
