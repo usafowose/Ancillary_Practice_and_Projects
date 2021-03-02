03a65fc0904c8694b89acf40c4d69dee2f282cd6
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2021-03-01 23:35:19 -0500

    Added new solution for working with non-primitive types

---
+++ C#/Non_Primitive_Types/Classes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Classes
+{
+    public class Person
+    {
+        public string Name;
+        public byte Age;
+        public char Gender;
+        public byte Weight;
+
+        public static int Walk(int distance) => distance;
+
+        public string Talk() 
+        {
+            return $"{this.Name} says Hello!"; 
+        }
+
+        public string Introduce() 
+        {
+            return $"Hello! My name is {this.Name}. I am a {this.Age} years old {this.Gender}. I weigh {this.Weight} lbs.";
+        }
+
+    }
+
+    public class Program 
+    {
+        static void Main() 
+        {
+            Person drew = new Person
+            {
+                Name = "Andrew",
+                Age = 23,
+                Gender = 'M',
+                Weight = 230
+            };
+
+            Console.WriteLine(Person.Walk(20));
+            Console.WriteLine(drew.Talk());
+            Console.WriteLine(drew.Introduce()); 
+        }
+    }
+    
+}
---
+++ C#/Non_Primitive_Types/Non_Primitive_Types/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Non_Primitive_Types
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
