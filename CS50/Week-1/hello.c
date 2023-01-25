ffe7284618a14fa24e87cebea69abf676504867b
Andrew Fowose <usafowose@gmail.com>
2023-01-25 17:22:35 -0500

    loops and bool in C

--- CS50/Week-1/hello.c
+++ CS50/Week-1/hello.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+ //standard input and output. header file. like importing a library. only include fnctly to use certain fns
 
 int main(void)
 {
---
+++ CS50/Week-1/meow.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("Meow\n");
+  printf("Meow\n");
+  printf("Meow\n");
+
+  // int counter = 0;
+  // counter += 2;
+  // printf("Number is %d", counter);
+
+  // LOOPS
+  int counter = 3;
+  while (counter > 0)
+  {
+    printf("meow\n");
+    counter--;
+  }
+}
