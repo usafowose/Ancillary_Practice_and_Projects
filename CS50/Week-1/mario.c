a6e8c4fabad25fc066a936b04940cea3598cbc80
Andrew Fowose <usafowose@gmail.com>
2023-01-30 15:30:01 -0500

    modified c program

--- CS50/Week-1/mario.c
+++ CS50/Week-1/mario.c
@@ -2,16 +2,7 @@
 
 int main(void)
 {
-  // printf("????\n");
-  for (int i = 0; i < 3; i++)
-  {
-    printf("#\n");
-  }
-  // printf("\n");
-
-  // print 3 by 3 grid
-
-  const int length = 5;
+  int length = 5;
   for (int i = 0; i < length; i++)
   {
     for (int j = 0; j < length; j++)
